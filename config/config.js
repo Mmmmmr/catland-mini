6332d16e2c2f9cfaccbf14eebe8fb51c6f4ea691
JLM <jianglimin999@gmail.com>
2020-03-06 22:26:32 +0800

    'add'

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
     // baseUrl: 'http://jiabandog.top:3000/v1'
-    baseUrl: 'http://catisland.ciliuciliu.club/v1',
-    url: 'http://catisland.ciliuciliu.club/'
+    baseUrl: 'https://catisland.ciliuciliu.club/v1',
+    url: 'https://catisland.ciliuciliu.club/'
     // baseUrl: 'http://localhost:3000/v1',
     // url: 'http://localhost:3000/'
 }
